3381ee7eae7a858e264b64b1e5aa2b086d56e8b5
cho wulf <fw190@hanmail.net>
2022-10-06 10:05:32 +0900

    first commit

---
+++ src/main/java/com/fw190/simple/Simple1Application.java
@@ -0,0 +1,21 @@
+package com.fw190.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Simple1Application {
+	
+	  @RequestMapping("/")
+	  public String home() {
+	    return "Hello Docker World";
+	  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simple1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fw190/simple/Simple1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fw190.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simple1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
